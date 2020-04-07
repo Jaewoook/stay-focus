b65a78d5e5096afe55ef9e069eebfbf0e0b93b05
Jaewook Ahn <ajw4586@gmail.com>
2020-04-07 17:37:24 +0900

    Improve: ListItem design and make it work

--- settings/src/components/ListItem.tsx
+++ settings/src/components/ListItem.tsx
@@ -1,22 +1,36 @@
 import React from "react";
-import { Button, Flex, Icon, Input, Typo, theme } from "@salesboost/malta";
+import { Block, Button, Flex, Icon, Input, Typo, theme } from "@salesboost/malta";
+import { Settings } from "../utils";
 
 interface ListItemProps {
     url: string;
     site: string;
 }
 
-export const ListItem = ({ site, url }: ListItemProps) => (
-    <Flex alignItems="center" style={{ padding: "8px 4px" }}>
-        <Typo name="body_181818_nv90_500">{site}</Typo>
-        <Typo name="caption_121212_nv40_300" ml="6px">{url}</Typo>
-    </Flex>
-);
+export const ListItem = ({ site, url }: ListItemProps) => {
+    const handleCloseClick = React.useCallback(() => {
+        Settings.removeBlockSite(site, url);
+    }, [site, url]);
+    return (
+        <Flex width="100%" alignItems="center" style={{ padding: "8px 4px" }}>
+            <Flex flex={1} alignItems="baseline">
+                <Typo maxWidth="100px" name="body_181818_nv90_500"
+                    style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>{site}</Typo>
+                <Typo maxWidth="175px" name="caption_121212_nv40_300"
+                    style={{ marginLeft: "6px", whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}>{url}</Typo>
+            </Flex>
+            <Block cursor="pointer" onClick={handleCloseClick}>
+                <Icon name="close" size={12} color={theme.colors.navy._40} />
+            </Block>
+        </Flex>
+    );
+};
 
 export const NewItem = () => {
     const [url, setUrl] = React.useState<string>("");
     const [site, setSite] = React.useState<string>("");
     const [active, setActive] = React.useState<boolean>(false);
+    const isAlreadyExists = React.useMemo(() => Settings.blockSites.includes({ title: site, url }), [site, url]);
 
     const onNewItemClick = React.useCallback(() => {
         const nextState = !active;
@@ -28,28 +42,45 @@ export const NewItem = () => {
         }
         setActive(nextState);
     }, [active, setActive, site, url]);
+
+    const handleAddClick = React.useCallback(() => {
+        Settings.addBlockSite(site, url);
+        setSite("");
+        setUrl("");
+    }, [site, url, setSite, setUrl]);
     return (
         <Flex flexDirection="column">
-            <Flex alignItems="center" onClick={onNewItemClick} cursor="pointer">
-                {active ? <>
-                    <Icon name="close" size={12} color={theme.colors.blue._80} mr="4px" />
-                    <Typo name="body_161616_nv80_500" color={theme.colors.blue._80} ml="4px">취소</Typo>
-                </> : <>
-                    <Icon name="add" size={12} color={theme.colors.blue._80} mr="4px" />
-                    <Typo name="body_161616_nv80_500" color={theme.colors.blue._80} ml="4px">현재 사이트 추가</Typo>
-                </>}
-            </Flex>
+            {active ? (
+                <Flex style={{ marginLeft: "auto" }} alignItems="center" onClick={onNewItemClick} cursor="pointer">
+                    <Icon name="close" size={12} color="rgba(57, 67, 226, 0.6)" mr="4px" />
+                    <Typo name="body_161616_nv80_500" color="rgba(57, 67, 226, 0.6)" ml="4px">취소</Typo>
+                </Flex>
+            ) : (
+                <Flex style={{ margin: "0 auto" }} alignItems="center" onClick={onNewItemClick} cursor="pointer">
+                    <Icon name="add" size={12} color="rgba(57, 67, 226, 0.6)" mr="4px" />
+                    <Typo name="body_161616_nv80_500" color="rgba(57, 67, 226, 0.6)" ml="4px">현재 사이트 추가</Typo>
+                </Flex>
+            )}
             {active ? (
                 <Flex flexDirection="column" style={{ marginTop: "4px" }}>
                     <Flex alignItems="center">
-                        <Typo name="body_141414_nv60_300">이름</Typo>
-                        <Input ml="8px" flex={1} value={site} />
+                        <Typo name="caption_141212_nv80_500">이름</Typo>
+                        <Input ml="8px" flex={1}
+                            onTextChange={(text) => setSite(text)}
+                            placeholder="YouTube"
+                            value={site} />
                     </Flex>
                     <Flex alignItems="center" style={{ marginTop: "8px" }}>
-                        <Typo name="body_141414_nv60_300">URL</Typo>
-                        <Input ml="8px" flex={1} value={url} />
+                        <Typo name="caption_141212_nv80_500">URL</Typo>
+                        <Input ml="8px" flex={1}
+                            onTextChange={(text) => setUrl(text)}
+                            placeholder="youtube.com"
+                            value={url} />
                     </Flex>
                     <Button
+                        mt={3} height="40px" fontSize="12px"
+                        disabled={isAlreadyExists || !site || !url}
+                        onClick={handleAddClick}
                         label="추가" />
                 </Flex>
             ) : null}
