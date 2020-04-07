529a3d0443962e2d8bbe28154b8ada7bb3f3f87e
Jaewook Ahn <ajw4586@gmail.com>
2020-04-07 17:38:01 +0900

    Improve: show block list on popup

--- settings/src/App.tsx
+++ settings/src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Flex, Icon, Split, Typo } from "@salesboost/malta";
-import { Layout, NewItem } from "./components";
+import { Layout, NewItem, ListItem } from "./components";
+import { Settings, SiteInfo } from "./utils";
 
 const App = () => {
+    const [blockList, setBlockList] = React.useState<SiteInfo[]>([]);
+    React.useEffect(() => {
+        setBlockList(Settings.blockSites || []);
+        chrome.runtime.onMessage.addListener((request, sender, sendRespond) => {
+            if (request.UPDATE_SETTINGS) {
+                setBlockList(Settings.blockSites || []);
+            }
+        });
+    }, []);
     return (
         <Layout>
             <Flex width="300px" flexDirection="column">
                 <Typo name="title_242018_nv90_500">Stay FOCUS</Typo>
                 <Split my="8px" horizontal length="100%" />
-                <Flex flexDirection="column">
+                <Flex flexDirection="column" style={{ marginTop: "4px" }}>
+                    {blockList.map(({ url, title }, i) => <ListItem key={i} site={title} url={url} />)}
                     <NewItem />
                 </Flex>
             </Flex>
