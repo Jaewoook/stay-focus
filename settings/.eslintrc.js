9594dda1baeef4e3b184c79214e25b07fd15ed10
Jaewook Ahn <ajw4586@gmail.com>
2020-04-12 14:50:32 +0900

    Update: settings eslint config

--- settings/.eslintrc.js
+++ settings/.eslintrc.js
@@ -1,41 +1,42 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:react/recommended",
         "plugin:@typescript-eslint/eslint-recommended"
     ],
-    "globals": {
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    globals: {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        ecmaVersion: 2018,
+        sourceType: "module"
     },
-    "plugins": [
+    plugins: [
         "react",
         "@typescript-eslint"
     ],
-    "rules": {
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    rules: {
+    },
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            rules: {
+                "@typescript-eslint/no-unused-vars": [2, { args: "none" }]
+            }
+        }
+    ]
 };
